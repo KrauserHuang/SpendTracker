8f560e0df6b2b2d4095a37127806f0a4b0c446dc
krauserhuang <crescentdence@gmail.com>
2021-11-15 16:57:16 +0800

    TestViewController change print statement

--- SpendTracker/Extension/TestViewController.swift
+++ SpendTracker/Extension/TestViewController.swift
@@ -12,6 +12,7 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("123")
+        print("一直失敗")
         
         // Do any additional setup after loading the view.
     }
